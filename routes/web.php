3c34619e23627cffe4e40fe12fb1355ec1eb0b5f
taiht <Hdt13102k3@gmail.com>
2024-03-29 10:34:17 +0700

    Part 2: Create routes dashboard

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\ApplicationController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -16,6 +16,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('admin/dashboard', function (){
-    return view('dashboard');
-});
+//Route::get('admin/dashboard', function (){
+//    return view('dashboard');
+//});
+
+Route::get('{view}', ApplicationController::class)->where('view', '(.*)');
