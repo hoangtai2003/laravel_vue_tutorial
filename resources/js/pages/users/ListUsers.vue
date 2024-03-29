d82bb6a1dad64adbf5df8f621727dd4a70935cc0
taiht <Hdt13102k3@gmail.com>
2024-03-29 21:51:03 +0700

    Part 4: Show List User

--- resources/js/pages/users/ListUsers.vue
+++ resources/js/pages/users/ListUsers.vue
@@ -8,17 +8,51 @@
                 <div class="col-sm-6">
                     <ol class="breadcrumb float-sm-right">
                         <li class="breadcrumb-item"><a href="#">Home</a></li>
-                        <li class="breadcrumb-item active">Starter Page</li>
+                        <li class="breadcrumb-item active">ListUsers</li>
                     </ol>
                 </div>
             </div>
         </div>
     </div>
-
-
     <div class="content">
+        <div class="container-fluid">
+            <div class="card">
+                <div class="card-body">
+                    <table class="table table-bordered">
+                        <thead>
+                        <tr>
+                            <th scope="col">#</th>
+                            <th scope="col">Name</th>
+                            <th scope="col">Email</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        <tr v-for="user in users" :key="user.id">
+                            <th scope="row">{{user.id}}</th>
+                            <td>{{user.name}}</td>
+                            <td>{{user.email}}</td>
+                        </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
 
     </div>
 </template>
 <script setup>
+    import axios from "axios";
+    import {onMounted, ref} from "vue";
+    let users = ref([])
+    const getUser = () => {
+        axios.get('/api/users')
+            // Sau khi nhận được respone thì gán giá trị response vào users thông qua .value
+            .then((response) => {
+                users.value = response.data
+        })
+    }
+    // Gọi lại hàm getUser() khi component được mounted
+    onMounted(() => {
+        getUser()
+    })
 </script>
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ApplicationController;
+use App\Http\Controllers\UserController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -16,8 +17,6 @@ use App\Http\Controllers\ApplicationController;
 Route::get('/', function () {
     return view('welcome');
 });
-//Route::get('admin/dashboard', function (){
-//    return view('dashboard');
-//});
+Route::get('api/users', [UserController::class, 'index']);
 
 Route::get('{view}', ApplicationController::class)->where('view', '(.*)');
