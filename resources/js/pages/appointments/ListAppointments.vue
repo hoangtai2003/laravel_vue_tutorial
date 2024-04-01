4af780a8c24ecc15315b6470c1f6b333d6215e12
taiht <Hdt13102k3@gmail.com>
2024-04-01 10:11:19 +0700

    Part 23: Start working on Appointment

--- resources/js/pages/appointments/ListAppointments.vue
+++ resources/js/pages/appointments/ListAppointments.vue
@@ -17,8 +17,73 @@
 
 
     <div class="content">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="d-flex justify-content-between mb-2">
+                        <div>
 
+                                <button class="btn btn-primary"><i class="fa fa-plus-circle mr-1"></i> Add New Appointment</button>
+
+                        </div>
+                        <div class="btn-group">
+                            <button  type="button" class="btn btn-secondary">
+                                <span class="mr-1">All</span>
+                                <span class="badge badge-pill badge-info">1</span>
+                            </button>
+
+                            <button type="button" class="btn btn-default">
+                                <span class="mr-1">Scheduled</span>
+                                <span class="badge badge-pill badge-primary">0</span>
+                            </button>
+                            <button type="button" class="btn btn-default">
+                                <span class="mr-1">Closed</span>
+                                <span class="badge badge-pill badge-success">1</span>
+                            </button>
+                        </div>
+                    </div>
+                    <div class="card">
+                        <div class="card-body">
+                            <table class="table table-bordered">
+                                <thead>
+                                <tr>
+                                    <th scope="col">#</th>
+                                    <th scope="col">Client Name</th>
+                                    <th scope="col">Date</th>
+                                    <th scope="col">Time</th>
+                                    <th scope="col">Status</th>
+                                    <th scope="col">Options</th>
+                                </tr>
+                                </thead>
+                                <tbody>
+                                <tr >
+                                    <td></td>
+                                    <td></td>
+                                    <td></td>
+                                    <td></td>
+                                    <td>
+                                            <span class="badge badge-success" >closed</span>
+                                    </td>
+                                    <td>
+                                        <a href="#">
+                                            <i class="fa fa-edit mr-2 text-primary"></i>
+                                        </a>
+
+                                        <a href="#">
+                                            <i class="fa fa-trash text-danger"></i>
+                                        </a>
+                                    </td>
+                                </tr>
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
+
+
 </template>
 <script setup>
 </script>
